a9f163dbcb15b6b5d4cb86e7abe59547a45ac7b2
Jake Mirra <jpluto909@gmail.com>
2020-11-02 15:41:50 -0600

    Remove test component.

--- src/index.js
+++ src/index.js
@@ -1,27 +1,8 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import RubiksApp from './components/RubiksApp';
-import RubiksAppContainer from './components/RubiksAppContainer';
+import RubiksApp from './components/RubiksApp/RubiksApp';
+import RubiksAppContainer from './components/RubiksApp/RubiksAppContainer';
 import './index.css';
-
-class TestComponent extends Component {
-  constructor() {
-    super();
-    console.log("TestComponent constructor.");
-  }
-
-  render() {
-    return (
-      <div>
-        <h1>
-          Hello World!
-        </h1>
-      </div>
-    );
-  }
-}
-
-console.log("We are here.");
 
 ReactDOM.render(
   <RubiksAppContainer />,
